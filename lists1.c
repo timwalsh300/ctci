9ff1a86e7070a17db79d4ea8d775ddaab7fceef9
Tim Walsh <timwalsh300@gmail.com>
2019-05-16 12:23:56 -0400

    finished linked-lists 1

--- lists1.c
+++ lists1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct node {
     int value;
@@ -26,8 +27,23 @@ void prepend(struct node *list, int v)
     list->next = addition;
 }
 
+int table_member(int t[], int min, int v)
+{
+    if (t[v - min]) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void mark_table(int t[], int min, int v)
+{
+    t[v - min] = 1;
+}
+
 int remove_duplicates(struct node *list)
 {
+    // start by scanning the list once to learn its properties
     int length = 0;
     int min = 100;
     int max = 0;
@@ -42,9 +58,27 @@ int remove_duplicates(struct node *list)
         }
         length++;
     }
-
     printf("length: %d min: %d max: %d\n", length, min, max);
-    return length;
+
+    // now start removing duplicates
+    int range = max - min + 1;
+    int table[range];
+    memset(table, 0, (range * 4));
+    int removals = 0;
+    current = list;
+    struct node *last;
+    while (current->next != NULL) {
+        current = current->next;
+        if (table_member(table, min, current->value)) {
+            last->next = current->next;
+            removals++;
+        } else {
+            mark_table(table, min, current->value);
+            last = current;
+        }
+    }
+
+    return removals;
 }
 
 void print_list(struct node *list)
@@ -58,19 +92,23 @@ void print_list(struct node *list)
 
 int main(void)
 {
-    struct node *list = malloc(sizeof(struct node));
-    list->next = NULL;
+    struct node list;
+    list.next = NULL;
+    struct node *listp = &list;
 
     unsigned char buffer[4];
     FILE *fp = fopen("/dev/urandom", "r");
     for (int i = 0; i < 20; i++) {
         fgets(buffer, 4, fp);
-        prepend(list, ((int) *buffer) % 100);
+        prepend(listp, ((int) *buffer) % 100);
     }
     fclose(fp);
-    printf("random list: ");
-    print_list(list);
+    printf("random list:\t");
+    print_list(listp);
     printf("\n");
 
-    remove_duplicates(list);
+    printf("%d duplicates removed\n", remove_duplicates(listp));
+    printf("resulting list:\t");
+    print_list(listp);
+    printf("\n");
 }
